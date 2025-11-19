9a55ecb6b61e8d077defb52a21cd387dbad55f33
HeatrixHub <hannes@heatrix.de>
2025-11-19 11:58:34 +0100

    Add top-level package initializer

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for the Berechnungstool application suite."""
+
+from . import app, Isolierung, SoffeigenschaftenLuft
+
+__all__ = [
+    "app",
+    "Isolierung",
+    "SoffeigenschaftenLuft",
+]
